fba092b883c37362ce7e9838e38416eebe0f6693
Curt Odar <curoda@users.noreply.github.com>
2022-02-16 15:41:23 -0500

    Update MMULT_app.py
    formatting update sent from Tom to Gary on Feb 16

--- MMULT_app.py
+++ MMULT_app.py
@@ -53,7 +53,7 @@ def Read_and_Display(key=0):
         mat1 = df1_complex.to_numpy()
 
         # display it
-        dfAB_complex = pd.DataFrame(mat1).applymap(lambda z: "%0.3f + %0.3fi" % (z.real, z.imag))
+        dfAB_complex = pd.DataFrame(mat1).applymap(lambda z: "%0.3f %+0.3fi" % (z.real, z.imag))
         st.write(dfAB_complex)
         return mat1
     else:
@@ -83,7 +83,7 @@ def main():
         matABC = matA@matB@matC
 
         # display the result
-        dfABC_complex = pd.DataFrame(matABC).applymap(lambda z: "%0.3f + %0.3fi" % (z.real, z.imag))
+        dfABC_complex = pd.DataFrame(matABC).applymap(lambda z: "%0.3f %+0.3fi" % (z.real, z.imag))
         st.write(dfABC_complex)
 
         # download the result
